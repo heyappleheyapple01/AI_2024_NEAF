32091a1342e3a40ba1fce45dd20b6d56efa503b4
heyappleheyapple01 <heyappleheyapple01@outlook.com>
2024-08-06 13:21:02 +0800

    0806

--- summer_lesson/classification_practice.py
+++ summer_lesson/classification_practice.py
@@ -3,26 +3,46 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 ## first part : Create data
-"""
-your code
-"""
+n_data = torch.ones(100, 2)
+x0 = torch.normal(2 * n_data, 1)   # torch.Size([100, 2])
+y0 = torch.zeros(100)              # label = 0
+x1 = torch.normal(-2 * n_data, 1)  # torch.Size([100, 2])
+y1 = torch.ones(100)               # label = 1 
+x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # torch.Size([100, 2])
+y = torch.cat((y0, y1), ).type(torch.LongTensor)    # torch.Size([200])
+
 
 ## second part : Create NN
-"""
-basic form:
+
 class Net(torch.nn.Module): # class a Network and input a torch module
-    def __init__(self, n_feature, n_hidden, n_output):
+    def __init__(self, n_feature, n_output):
         super(Net, self).__init__() # To inherit things from Net, the standard process must be added
 
     def forward(self, x):
         
         return x 
 
-optimizer = torch.optim.SGD()
-loss_func = torch.nn.MSELoss()
-"""
+net = Net(n_feature=2, n_output=2) # define the network
+print(net) # network architecture
 
-## third part : training
-"""
-your code
-"""
+optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
+loss_func = torch.nn.CrossEntropyLoss() # 已經包含了 sigmoid or softmax
+
+
+plt.ion()
+
+for t in range(100):
+    
+    
+    if t % 5 == 0:
+        plt.cla()
+        prediction = torch.max(out, 1)[1]    
+        pred_y = prediction.data.numpy()     # convert tensor to numpy
+        target_y = y.data.numpy()            # convert tensor to numpy
+        plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
+        accuracy = float((pred_y == target_y).astype(int).sum()) / float(target_y.size)   # 預測正確除以總數量
+        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+        
+plt.ioff()
+plt.show()
--- summer_lesson/regression_practice.py
+++ summer_lesson/regression_practice.py
@@ -3,13 +3,11 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 ## first part : Create data
-"""
-your code
-"""
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1) # 增加一個維度！pytorch 中會需要多增加一個維度
+y = x.pow(2) + 0.2 * torch.rand(x.size()) # add some noise
 
 ## second part : Create NN
-"""
-basic form:
+
 class Net(torch.nn.Module): # class a Network and input a torch module
     def __init__(self, n_feature, n_hidden, n_output):
         super(Net, self).__init__() # To inherit things from Net, the standard process must be added
@@ -17,12 +15,24 @@ class Net(torch.nn.Module): # class a Network and input a torch module
     def forward(self, x):
         
         return x 
+    
+net = Net(n_feature=1, n_hidden=10, n_output=1) 
+print(net) # network architecture
 
-optimizer = torch.optim.SGD()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
 loss_func = torch.nn.MSELoss()
-"""
 
-## third part : training
-"""
-your code
-"""
+plt.ion() # something about plotting
+
+for t in range(200):
+    
+
+    if t % 5 == 0: 
+        plt.cla()
+        plt.scatter(x.data.numpy(), y.data.numpy())
+        plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+        plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
